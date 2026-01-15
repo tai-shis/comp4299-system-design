1900128d170993c74a79b22b9aca79c6f05171de
Ty Wagner <tywagner04@gmail.com>
2026-01-15 10:48:15 -0700

    Update config to handle MDX text formatting.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+import createMDX from '@next/mdx';
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} **/
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
+  // Optionally, add any other Next.js config below
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
