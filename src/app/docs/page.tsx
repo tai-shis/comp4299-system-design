ce38d1da3dccd99056d8875073b4edea37f7a160
Ty Wagner <tywagner04@gmail.com>
2026-01-22 16:56:37 -0700

    Missed some file changes in the last commit

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,7 @@
+// I think this is how to do it? Not sure tbh. 
+// No red squigglies in VSCode though
+import mdxContent from '@/markdown/docs.mdx';
+
+export default function Page() {
+    return mdxContent;
+}
