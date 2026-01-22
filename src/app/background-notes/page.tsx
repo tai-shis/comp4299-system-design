e682c7b2386b7bf8710442fed5872d50bc623700
Ty Wagner <tywagner04@gmail.com>
2026-01-22 16:35:46 -0700

    Fixed file hierarchy to use .tsx files for pages and import .mdx rather than the other way around.

---
+++ src/app/background-notes/page.tsx
@@ -0,0 +1,7 @@
+// I think this is how to do it? Not sure tbh. 
+// No red squigglies in VSCode though
+import mdxContent from '@/markdown/background-notes.mdx';
+
+export function Page() {
+    return mdxContent;
+}
