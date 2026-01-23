3bed420ff9565761a3d0c0cec9673d21ca3b0c6b
Ty Wagner <tywagner04@gmail.com>
2026-01-22 17:02:00 -0700

    Changed .mdx import into .tsx file. Hopefully Vercel likes this change.

--- src/app/background-notes/page.tsx
+++ src/app/background-notes/page.tsx
@@ -1,7 +1,7 @@
 // I think this is how to do it? Not sure tbh. 
 // No red squigglies in VSCode though
-import mdxContent from '@/markdown/background-notes.mdx';
+import Background_Notes from '@/markdown/background-notes.mdx';
 
 export function Page() {
-    return mdxContent;
+    return Background_Notes;
 }
--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,7 +1,7 @@
 // I think this is how to do it? Not sure tbh. 
 // No red squigglies in VSCode though
-import mdxContent from '@/markdown/docs.mdx';
+import Docs from '@/markdown/docs.mdx';
 
 export default function Page() {
-    return mdxContent;
+    return Docs;
 }
